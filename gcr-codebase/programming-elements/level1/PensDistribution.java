2afdc3ad33c89c4cc49ef33378658d4cd0e4e0e2
Teeshakul <teesha119@gmail.com>
2026-06-09 21:22:47 +0530

    [Teesha] Add PensDistribution solution

---
+++ gcr-codebase/programming-elements/level1/PensDistribution.java
@@ -0,0 +1,6 @@
+public class PensDistribution {
+    public static void main(String[] args) {
+        System.out.println("Pens per student = " + (14 / 3));
+        System.out.println("Remaining pens = " + (14 % 3));
+    }
+}
