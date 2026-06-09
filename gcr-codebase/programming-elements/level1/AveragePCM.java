d850f22ebcc003aeb36516b6eac8765b7a7e25ba
Teeshakul <teesha119@gmail.com>
2026-06-09 21:21:54 +0530

    [Teesha] Add AveragePCM solution

---
+++ gcr-codebase/programming-elements/level1/AveragePCM.java
@@ -0,0 +1,6 @@
+public class AveragePCM {
+    public static void main(String[] args) {
+        double avg = (94 + 95 + 96) / 3.0;
+        System.out.println("Sam's average mark in PCM is " + avg);
+    }
+}
