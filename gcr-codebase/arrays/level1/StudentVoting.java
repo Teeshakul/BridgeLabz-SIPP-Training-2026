0953cf7ac66f8e47407aedfbe90857e4d211cc5e
Teeshakul <teesha119@gmail.com>
2026-06-11 23:42:41 +0530

    [Teesha] Add StudentVoting solution

---
+++ gcr-codebase/arrays/level1/StudentVoting.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class StudentVoting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] age = new int[10];
+
+        for (int i = 0; i < age.length; i++) {
+            age[i] = sc.nextInt();
+
+            if (age[i] < 0)
+                System.out.println("Invalid Age");
+            else if (age[i] >= 18)
+                System.out.println("Can Vote");
+            else
+                System.out.println("Cannot Vote");
+        }
+    }
+}
