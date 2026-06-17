f39aeaa44b5d7029f93b4be7a4eddf9c4a8b5311
Teeshakul <teesha119@gmail.com>
2026-06-18 03:30:43 +0530

    [Teesha] Add ReverseNumberArray solution

---
+++ gcr-codebase/arrays/level2/ReverseNumberArray.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ReverseNumberArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int number = sc.nextInt();
+
+        int[] digits = new int[20];
+        int count = 0;
+
+        while(number != 0){
+            digits[count++] = number % 10;
+            number /= 10;
+        }
+
+        for(int i=0;i<count;i++){
+            System.out.print(digits[i]);
+        }
+    }
+}
