b362af62872adf9394cc61049a27f80839c35550
Teeshakul <219322852+Teeshakul@users.noreply.github.com>
2026-06-25 09:52:00 +0530

    [Teesha] Add RestaurantDemo solution

---
+++ gcr-codebase/inheritance/scenario-based/RestaurantDemo.java
@@ -0,0 +1,46 @@
+class Person {
+    String name;
+    int id;
+
+    Person(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+}
+
+interface Worker {
+    void performDuties();
+}
+
+class Chef extends Person implements Worker {
+
+    Chef(String name, int id) {
+        super(name, id);
+    }
+
+    public void performDuties() {
+        System.out.println(name + " is cooking food.");
+    }
+}
+
+class Waiter extends Person implements Worker {
+
+    Waiter(String name, int id) {
+        super(name, id);
+    }
+
+    public void performDuties() {
+        System.out.println(name + " is serving customers.");
+    }
+}
+
+public class RestaurantDemo {
+    public static void main(String[] args) {
+
+        Chef chef = new Chef("Rahul", 1);
+        Waiter waiter = new Waiter("Aman", 2);
+
+        chef.performDuties();
+        waiter.performDuties();
+    }
+}
